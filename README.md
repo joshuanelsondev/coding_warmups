05d1db9eb6b429acc0cb349b88869d4f95de0e64
joshuanelsondev <joshuanelsondev@gmail.com>
2024-01-22 18:51:14 -0500

    Add folders and first warmup, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# coding_warmups
+# Coding Warmups
+Hey Coaches, feel free to fork this repo and use any of the coding warmups for your classes and please make pull requests and contribute any warmups that you've used. You can add your own warmups or add ones that you've used from other platforms. All are welcome! 
+
+Add any folders that I may have missed or any that you think are important. Thank You!
---
+++ strings/separateByCapitals.js
@@ -0,0 +1,10 @@
+/* 
+Given a string of words containing capitalized words, return a new string where each word that's capitalized is separated by a comma.
+@params {string} = string with capitalized words inside
+@returns {string} = string with the capitalized words separated
+
+examples:
+'WelcomeToPursuitInQueensNewYork' => 'Welcome, To, Pursuit, In, Queens, New, York';
+
+'TheCowJumpedOverTheMoon' => 'The, Cow, Jumped, Over, The, Moon'
+*/
